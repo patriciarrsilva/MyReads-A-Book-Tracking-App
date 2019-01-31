0abc864826dfb616352faaae59420a80d242210b
Patrícia <prrs89@gmail.com>
2019-01-31 19:42:23 +0000

    Updates

---
+++ precache-manifest.15348e895fe04372bcb14d55dc37852b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads-A-Book-Tracking-App/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads-A-Book-Tracking-App/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads-A-Book-Tracking-App/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "bdabf07b7334ceba105e",
+    "url": "/MyReads-A-Book-Tracking-App/static/js/runtime~main.bdabf07b.js"
+  },
+  {
+    "revision": "218bc786a45233065553",
+    "url": "/MyReads-A-Book-Tracking-App/static/js/main.218bc786.chunk.js"
+  },
+  {
+    "revision": "55561c7873a9919762be",
+    "url": "/MyReads-A-Book-Tracking-App/static/js/1.55561c78.chunk.js"
+  },
+  {
+    "revision": "218bc786a45233065553",
+    "url": "/MyReads-A-Book-Tracking-App/static/css/main.fa33ae1f.chunk.css"
+  },
+  {
+    "revision": "6594bd1992dc36f78599ff12c3b25d5d",
+    "url": "/MyReads-A-Book-Tracking-App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MyReads-A-Book-Tracking-App/precache-manifest.15348e895fe04372bcb14d55dc37852b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MyReads-A-Book-Tracking-App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
